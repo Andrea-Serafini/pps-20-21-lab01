60d2563710c399d82f72d226105b089bdd34290a
Andrea Serafini <andrea.serafini11@studio.unibo.it>
2021-03-22 12:06:22 +0100

    Changed assertion import to static

--- pps-lab01-tdd/test/CircularListTest.java
+++ pps-lab01-tdd/test/CircularListTest.java
@@ -1,9 +1,10 @@
 import lab01.tdd.*;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * The test suite for testing the CircularList implementation
@@ -38,28 +39,28 @@ public class CircularListTest {
     public void testAddSize(){
         addElements(0);
 
-        Assertions.assertEquals(1,list.size());
+        assertEquals(1,list.size());
     }
 
     @Test
     public void testIsEmpty(){
-        Assertions.assertTrue(list.isEmpty());
+        assertTrue(list.isEmpty());
 
         addElements(0);
 
-        Assertions.assertFalse(list.isEmpty());
+        assertFalse(list.isEmpty());
     }
 
     @Test
     public void testNextOnEmpty(){
-        Assertions.assertEquals(Optional.empty(),list.next());
+        assertEquals(Optional.empty(),list.next());
     }
 
     @Test
     public void testNextOnFirst(){
         addElements(0);
 
-        Assertions.assertEquals(Optional.of(0),list.next());
+        assertEquals(Optional.of(0),list.next());
 
     }
 
@@ -67,147 +68,147 @@ public class CircularListTest {
     public void testNextOnLast(){
         addElements(0, 1);
 
-        Assertions.assertEquals(Optional.of(0),list.next());
-        Assertions.assertEquals(Optional.of(1),list.next());
-        Assertions.assertEquals(Optional.of(0),list.next());
+        assertEquals(Optional.of(0),list.next());
+        assertEquals(Optional.of(1),list.next());
+        assertEquals(Optional.of(0),list.next());
     }
 
     @Test
     public void testPreviousOnEmpty(){
-        Assertions.assertEquals(Optional.empty(),list.previous());
+        assertEquals(Optional.empty(),list.previous());
     }
 
     @Test
     public void testPreviousOnFirst(){
         addElements(0, 1);
 
-        Assertions.assertEquals(Optional.of(1),list.previous());
+        assertEquals(Optional.of(1),list.previous());
     }
 
     @Test
     public void testPreviousOnOneValue(){
         addElements(0);
 
-        Assertions.assertEquals(Optional.of(0),list.previous());
+        assertEquals(Optional.of(0),list.previous());
     }
 
     @Test
     public void testBothDirectionListIteration(){
         addElements(0, 1, 2, 3);
 
-        Assertions.assertEquals(Optional.of(0),list.next());
-        Assertions.assertEquals(Optional.of(3),list.previous());
-        Assertions.assertEquals(Optional.of(2),list.previous());
-        Assertions.assertEquals(Optional.of(1),list.previous());
-        Assertions.assertEquals(Optional.of(2),list.next());
+        assertEquals(Optional.of(0),list.next());
+        assertEquals(Optional.of(3),list.previous());
+        assertEquals(Optional.of(2),list.previous());
+        assertEquals(Optional.of(1),list.previous());
+        assertEquals(Optional.of(2),list.next());
     }
 
     @Test
     public void testReset(){
         addElements(0, 1);
 
-        Assertions.assertEquals(Optional.of(0),list.next());
+        assertEquals(Optional.of(0),list.next());
 
         list.reset();
 
-        Assertions.assertEquals(Optional.of(0),list.next());
+        assertEquals(Optional.of(0),list.next());
     }
 
     @Test
     public void testEvenStrategyOnEmpty(){
-        Assertions.assertEquals(Optional.empty(),list.next(even));
+        assertEquals(Optional.empty(),list.next(even));
     }
 
     @Test
     public void testMultipleOfStrategyOnEmpty(){
-        Assertions.assertEquals(Optional.empty(),list.next(multiple));
+        assertEquals(Optional.empty(),list.next(multiple));
     }
 
     @Test
     public void testEqualsStrategyOnEmpty(){
-        Assertions.assertEquals(Optional.empty(),list.next(equal));
+        assertEquals(Optional.empty(),list.next(equal));
     }
 
     @Test
     public void testEvenStrategy(){
         addElements(1, 1, 2, 4, 1);
 
-        Assertions.assertEquals(Optional.of(2),list.next(even));
-        Assertions.assertEquals(Optional.of(4),list.next(even));
-        Assertions.assertEquals(Optional.of(2),list.next(even));
+        assertEquals(Optional.of(2),list.next(even));
+        assertEquals(Optional.of(4),list.next(even));
+        assertEquals(Optional.of(2),list.next(even));
     }
 
     @Test
     public void testEvenStrategyNoMatch(){
         addElements(1, 3, 5, 5, 5);
 
-        Assertions.assertEquals(Optional.empty(),list.next(even));
-        Assertions.assertEquals(Optional.of(1),list.next());
-        Assertions.assertEquals(Optional.empty(),list.next(even));
-        Assertions.assertEquals(Optional.of(3),list.next());
+        assertEquals(Optional.empty(),list.next(even));
+        assertEquals(Optional.of(1),list.next());
+        assertEquals(Optional.empty(),list.next(even));
+        assertEquals(Optional.of(3),list.next());
     }
 
     @Test
     public void testMultipleOfStrategy(){
         addElements(1, 1, 3, 1, 9);
 
-        Assertions.assertEquals(Optional.of(3),list.next(multiple));
-        Assertions.assertEquals(Optional.of(9),list.next(multiple));
-        Assertions.assertEquals(Optional.of(3),list.next(multiple));
+        assertEquals(Optional.of(3),list.next(multiple));
+        assertEquals(Optional.of(9),list.next(multiple));
+        assertEquals(Optional.of(3),list.next(multiple));
     }
 
     @Test
     public void testMultipleOfStrategyNoMatch(){
         addElements(1, 5, 2);
 
-        Assertions.assertEquals(Optional.of(1),list.next());
-        Assertions.assertEquals(Optional.empty(),list.next(multiple));
-        Assertions.assertEquals(Optional.of(5),list.next());
+        assertEquals(Optional.of(1),list.next());
+        assertEquals(Optional.empty(),list.next(multiple));
+        assertEquals(Optional.of(5),list.next());
     }
 
     @Test
     public void testEqualsStrategy(){
         addElements(1, 1, 3, 5, 3);
 
-        Assertions.assertEquals(Optional.of(3),list.next(equal));
-        Assertions.assertEquals(Optional.of(3),list.next(equal));
-        Assertions.assertEquals(Optional.of(3),list.next(equal));
+        assertEquals(Optional.of(3),list.next(equal));
+        assertEquals(Optional.of(3),list.next(equal));
+        assertEquals(Optional.of(3),list.next(equal));
     }
 
     @Test
     public void testEqualsStrategyNoMatch(){
         addElements(1, 5, 2);
 
-        Assertions.assertEquals(Optional.of(1),list.next());
-        Assertions.assertEquals(Optional.empty(),list.next(equal));
-        Assertions.assertEquals(Optional.of(5),list.next());
+        assertEquals(Optional.of(1),list.next());
+        assertEquals(Optional.empty(),list.next(equal));
+        assertEquals(Optional.of(5),list.next());
     }
 
     @Test
     public void testOddStrategy(){
         addElements(1, 3, 2, 4, 5);
 
-        Assertions.assertEquals(Optional.of(1),list.next(odd));
-        Assertions.assertEquals(Optional.of(3),list.next(odd));
-        Assertions.assertEquals(Optional.of(5),list.next(odd));
+        assertEquals(Optional.of(1),list.next(odd));
+        assertEquals(Optional.of(3),list.next(odd));
+        assertEquals(Optional.of(5),list.next(odd));
     }
 
     @Test
     public void testNotMultipleOfStrategy(){
         addElements(1, 4, 3, 6, 9);
 
-        Assertions.assertEquals(Optional.of(1),list.next(notMultiple));
-        Assertions.assertEquals(Optional.of(4),list.next(notMultiple));
-        Assertions.assertEquals(Optional.of(1),list.next(notMultiple));
+        assertEquals(Optional.of(1),list.next(notMultiple));
+        assertEquals(Optional.of(4),list.next(notMultiple));
+        assertEquals(Optional.of(1),list.next(notMultiple));
     }
 
     @Test
     public void testUnequalsStrategy(){
         addElements(1, 1, 3, 5, 3);
 
-        Assertions.assertEquals(Optional.of(1),list.next(unequal));
-        Assertions.assertEquals(Optional.of(1),list.next(unequal));
-        Assertions.assertEquals(Optional.of(5),list.next(unequal));
+        assertEquals(Optional.of(1),list.next(unequal));
+        assertEquals(Optional.of(1),list.next(unequal));
+        assertEquals(Optional.of(5),list.next(unequal));
     }
 
     private void addElements(int ... values){
